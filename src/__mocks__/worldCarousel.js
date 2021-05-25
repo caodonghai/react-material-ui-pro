bf794faece24442a5d0b6b8bdb1db1ea46be37b3
東 <liangsheng.cao@tuya.com>
2021-05-25 14:15:45 +0800

    封装 并 添加文字轮播

---
+++ src/__mocks__/worldCarousel.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    data: '20100101',
+    address: '杭州',
+    content: '元旦抓娃娃开心啊',
+  },
+  {
+    data: '20200202',
+    address: '杭州',
+    content: '二月二，炒大豆',
+  },
+  {
+    data: '20210303',
+    address: '杭州',
+    content: '西湖美景，三月天哎',
+  },
+  {
+    data: '20210404',
+    address: '杭州',
+    content: '愚人节快乐',
+  },
+  {
+    data: '20210525 13:13:00',
+    address: '杭州',
+    content: '五月，我们来啦',
+  },
+  {
+    data: '20200606',
+    address: '杭州',
+    content: '六月的雨，下个不停...',
+  },
+  {
+    data: '20210707',
+    address: '杭州',
+    content: '七夕节快乐...',
+  },
+];
---
+++ src/components/WorldCarousel.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef } from 'react';
+import { experimentalStyled } from '@material-ui/core';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+moment.locale('zh-cn');
+
+const MainLayoutContent = experimentalStyled('div')({
+  margin: '0 5px',
+  overflow: 'hidden',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+function WorldCarousel({
+  dataList,
+  liHeight,
+  speed,
+}) {
+  const boxContentRef = useRef();
+  const ulRef = useRef();
+  const timerRef = useRef();
+
+  const marquee = () => {
+    const ulListLen = ulRef.current.children.length;
+    if (ulRef.current.offsetTop <= -(liHeight * (ulListLen - 1))) { // 判断复制的信息是否到达box的最左边
+      ulRef.current.style.top = 0;
+      ulRef.current.style.transition = 'none';
+      marquee();
+    } else {
+      ulRef.current.style.transition = 'all .5s ease-in-out';
+      ulRef.current.style.top = `${ulRef.current.offsetTop - liHeight}px`;
+    }
+  };
+
+  const setEffectFunction = () => {
+    timerRef.current = setInterval(marquee, speed * 1000);
+  };
+
+  const removeEffectFunction = () => {
+    clearInterval(timerRef.current);
+  };
+
+  useEffect(() => {
+    console.log({ boxContentRef });
+    timerRef.current = setInterval(marquee, speed * 1000); // 设置定时器
+    boxContentRef.current.onmouseenter = removeEffectFunction;
+    boxContentRef.current.onmouseleave = setEffectFunction;
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
+  const content = () => {
+    dataList?.push(dataList[0]);
+    return dataList.map((item, index) => (
+      <li
+        key={(index === (dataList.length - 1)) ? 'firstDataCope' : item.data}
+        style={{
+          width: '100%',
+          height: liHeight,
+          lineHeight: `${liHeight}px`,
+          textAlign: 'center',
+          fontSize: '18px',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap'
+        }}
+      >
+        <span>{ item.content }</span>
+        &nbsp;&nbsp;
+        <span>{ moment(item.data).fromNow() }</span>
+      </li>
+    ));
+  };
+
+  return (
+    <MainLayoutContent>
+      <div
+        ref={boxContentRef}
+        style={{
+          overflow: 'hidden',
+          height: `${liHeight}px`,
+          width: '100%',
+          border: 0,
+          position: 'relative'
+        }}
+      >
+        <ul
+          ref={ulRef}
+          style={{
+            width: '100%',
+            listStyleType: 'disc',
+            marginBlock: 0,
+            marginInline: 0,
+            paddingInline: 0,
+            position: 'absolute',
+            top: 0
+          }}
+        >
+          { content() }
+        </ul>
+      </div>
+    </MainLayoutContent>
+  );
+}
+
+WorldCarousel.propTypes = {
+  dataList: PropTypes.array, // 数据列表
+  liHeight: PropTypes.number, // 移动速度，值越大速度越慢
+  speed: PropTypes.number, // li列表的高度
+};
+
+WorldCarousel.defaultProps = {
+  dataList: [],
+  liHeight: 30,
+  speed: 3,
+};
+
+export default WorldCarousel;
